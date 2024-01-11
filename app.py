aa3bf9d5384a4b94894b674b11406524265e9c56
Daniel B <daniel.m.bassett@outlook.com>
2024-01-11 17:39:13 -0600

    .

--- app.py
+++ app.py
@@ -42,7 +42,6 @@ tp = TextProcessor()
 
 # st.set_page_config(layout='wide')
 
-@st.cache_data
 def load_data(path):
     if '.parquet' in path:
         return pd.read_parquet(path)
@@ -62,12 +61,10 @@ def concat_df(list_of_df):
 
 
 def process_df(df):
-    df = df.rename(columns={'parent_name': 'agency'})
-    df = df.rename(columns={'sbir_topic_link': 'url'})
-    df = df.rename(columns={'topic_title': 'title'})
     df = df.rename(columns={'opportunity_number': 'topic_number'})
     df = df.query('text.str.len() > 250')
     df = df.reset_index(drop=True)
+    df = df[['topic_number', 'closing_date', 'title', 'text', 'url', 'text_embedded', '2grams']]
     return df
 
 
@@ -187,170 +184,175 @@ if "button2" not in st.session_state:
     st.session_state["button2"] = False
 
 if "grant_recommendations" not in st.session_state:
-    st.session_state["grant_recommendations"] = pd.DataFrame()
+    st.session_state["grant_recommendations"] = False
 
 if "text_box_button" not in st.session_state:
     st.session_state['text_box_button'] = False
 
 st.session_state["download_csv"] = True
 
-# create sidebar menu with options
-selected = option_menu(
-    menu_title=None,
-    menu_icon='cast',
-    default_index=0,
-    options=['Drag & Drop', 'Text Box'],
-    orientation='horizontal',
-    icons=['droplet', 'card-text'],
-    styles= {'container': {
-                'font-size': '12px'
-    }}
-)
+# # create sidebar menu with options
+# selected = option_menu(
+#     menu_title=None,
+#     menu_icon='cast',
+#     default_index=0,
+#     options=['Drag & Drop', 'Text Box'],
+#     orientation='horizontal',
+#     icons=['droplet', 'card-text'],
+#     styles= {'container': {
+#                 'font-size': '12px'
+#     }}
+# )
 
 
-if selected == 'Drag & Drop':
+# if selected == 'Drag & Drop':
 
-    # Upload csv
-    uploaded_file  = st.file_uploader('Choose a file', accept_multiple_files=False)
+# Upload csv
+uploaded_file  = st.file_uploader('Choose a file', accept_multiple_files=False)
 
-    if uploaded_file is not None:
-        dataframe = read_file(uploaded_file)
+if uploaded_file is not None:
+    dataframe = read_file(uploaded_file)
 
-        # drop duplicate abstracts
-        dataframe = (dataframe
-                     .assign(temp_summary = lambda df: df['summary'].str.lower())
-                     .drop_duplicates(subset='temp_summary')
-                     .drop('temp_summary', axis=1)
-                     .reset_index(drop=True)
-                     )
+    # drop duplicate abstracts
+    dataframe = (dataframe
+                    .assign(temp_summary = lambda df: df['summary'].str.lower())
+                    .drop_duplicates(subset='temp_summary')
+                    .drop('temp_summary', axis=1)
+                    .reset_index(drop=True)
+                    )
 
-    col1, col2, col3, col4 = st.columns([3, 3, 5, 3])
+col1, col2, col3, col4 = st.columns([3, 3, 5, 3])
 
-    with col1:
-        find_program = st.button('Find Programs')
+with col1:
+    find_program = st.button('Find Programs')
 
-    # filter_columns = st.columns([5, 7])
-    # with filter_columns[0]:
-    #     with st.expander('Filters'):
-    #         agencies = st.multiselect(label='Choose Agency', options=grants['agency'].unique())
+# filter_columns = st.columns([5, 7])
+# with filter_columns[0]:
+#     with st.expander('Filters'):
+#         agencies = st.multiselect(label='Choose Agency', options=grants['agency'].unique())
 
-    if find_program:
-        st.session_state['button1'] = True
-        st.session_state['download_csv'] = False
+if find_program:
+    st.session_state['button1'] = True
+    st.session_state['download_csv'] = False
 
-    if st.session_state['button1'] and uploaded_file is not None and st.session_state['download_csv'] == False:
-        column_names = ['company', 'project_title', 'summary', 'agency', 'grant_title', 'url', 'text']
-        grant_recommendations = pd.DataFrame(columns=column_names)
-        for index, row in dataframe.iterrows():
-            company = row['company']
-            summary = row['summary']
-            title = row['title']
-            query_embedding = get_embedding(summary)
+st.write(grants)
 
-            # tokens and ngrams for abstract text
-            tokens = tp.generate_tokens(text=summary)
-            unigrams = tp.generate_ngrams(tokens, 1)
-            bigrams = tp.generate_ngrams(tokens, 2)
-            trigrams = tp.generate_ngrams(tokens, 3)
-            temp_df = (grants
-                      .assign(
-                          jaccard_unigram=grants['unigrams'].apply(lambda x: modified_jaccard(x, unigrams)),
-                          jaccard_bigram=grants['bigrams'].apply(lambda x: modified_jaccard(x, bigrams)),
-                          jaccard_trigram=grants['trigrams'].apply(lambda x: modified_jaccard(x, trigrams)),
-                          cosine_similarity=grants['text_embedded'].apply(lambda y: np.dot(query_embedding, y)),
-                          description_len=grants['text'].str.len()
-                          )
-                      .query('cosine_similarity >= 0.80 and jaccard_bigram != 0')
-                      .groupby(['topic_number'], as_index=False)
-                      .agg(
-                          title=('title', 'max'),
-                          topic_number=('topic_number', 'max'),
-                          text=('text', 'sum'),
-                          jaccard_bigram=('jaccard_bigram', 'max'),
-                          jaccard_trigram=('jaccard_trigram', 'max'),
-                          cosine_similarity=('cosine_similarity', 'max'),
-                          description_len=('description_len', 'median'),
-                          url=('url', 'max'),
-                          agency=('agency', 'max')
-                          )
-                    ).sort_values(by='cosine_similarity', ascending=False).head(3)
-            for index2, row2 in temp_df.iterrows():
-                agency = row2['agency']
-                grant_title = row2['title']
-                url = row2['url']
-                text = row2['text']
-                cosine_similarity = row2['cosine_similarity']
-                new_row = {'company': company, 'project_title': title, 'summary': summary, 'agency': agency, 'grant_title': grant_title, 'text': text, 'url': url}
-                new_row_df = pd.DataFrame([new_row])
-                grant_recommendations = pd.concat([grant_recommendations, new_row_df], ignore_index=True)
-        st.session_state["grant_recommendations"] = grant_recommendations
+if st.session_state['button1'] and uploaded_file is not None and st.session_state['download_csv'] == False:
+    column_names = ['company', 
+                    'project_title', 
+                    'summary', 
+                    # 'agency', 
+                    'grant_title', 
+                    'url', 
+                    'text']
+    grant_recommendations = pd.DataFrame(columns=column_names)
+    for index, row in dataframe.iterrows():
+        company = row['company']
+        summary = row['summary']
+        title = row['title']
+        query_embedding = get_embedding(summary)
 
-
-    if not st.session_state["grant_recommendations"].empty:
-        st.divider()
-        st.subheader('Grant Recommendations')
-        grant_recommendations = st.session_state["grant_recommendations"]
-        st.write(grant_recommendations)
-
-
-        excel_file = convert_df_to_excel(grant_recommendations)
-        
-
-        if st.download_button("Press to Download", excel_file, "grant_recommendations.xlsx", "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", key='download-excel'):
-            st.session_state['download_excel'] = True
-        # csv = convert_df_to_csv(grant_recommendations)
-        # if st.download_button("Press to Download",csv,"grant_recommendations.csv","text/csv", key='download-csv'):
-        #     st.session_state['download_csv'] = True
-
-
-if selected == 'Text Box':
-    # Text Input
-    query = st.text_area('Enter description:', max_chars=3000, key='query_text', height=300)
-
-    col1, col2, col3, col4 = st.columns([3, 3, 5, 3])
-
-    with col1:
-        find_program = st.button('Find Programs')
-    with col2:
-        delete_abstract = st.button('Clear Keywords', on_click=clear_query)
-
-    if find_program:
-        st.session_state['text_box_button'] = True
-
-    if st.session_state['text_box_button'] & len(query) > 0:
         # tokens and ngrams for abstract text
-        query_embedding = get_embedding(query)
-        tokens = tp.generate_tokens(text=query)
+        tokens = tp.generate_tokens(text=summary)
         unigrams = tp.generate_ngrams(tokens, 1)
         bigrams = tp.generate_ngrams(tokens, 2)
-        trigrams = tp.generate_ngrams(tokens, 3)
-
         temp_df = (grants
-                  .assign(
-                      jaccard_unigram=grants['unigrams'].apply(lambda x: modified_jaccard(x, unigrams)),
-                      jaccard_bigram=grants['bigrams'].apply(lambda x: modified_jaccard(x, bigrams)),
-                      jaccard_trigram=grants['trigrams'].apply(lambda x: modified_jaccard(x, trigrams)),
-                      cosine_similarity=grants['text_embedded'].apply(lambda y: np.dot(query_embedding, y)),
-                      description_len=grants['text'].str.len()
-                      )
-                  .query('cosine_similarity >= 0.80 and jaccard_bigram != 0')
-                  .groupby(['topic_number'], as_index=False)
-                  .agg(title=('title', 'max'),
-                       topic_number=('topic_number', 'max'),
-                       text=('text', 'sum'),
-                       jaccard_bigram=('jaccard_bigram', 'max'),
-                       jaccard_trigram=('jaccard_trigram', 'max'),
-                       cosine_similarity=('cosine_similarity', 'max'),
-                       description_len=('description_len', 'median'),
-                       url=('url', 'max')
-                       )
-                  ).sort_values(by='cosine_similarity', ascending=False)
-        
+                    .assign(
+                        jaccard_bigram=grants['2grams'].apply(lambda x: modified_jaccard(x, bigrams)),
+                        cosine_similarity=grants['text_embedded'].apply(lambda y: np.dot(query_embedding, y)),
+                        description_len=grants['text'].str.len()
+                        )
+                    .query('cosine_similarity >= 0.80 and jaccard_bigram != 0')
+                    .groupby(['topic_number'], as_index=False)
+                    .agg(
+                        title=('title', 'first'),
+                        topic_number=('topic_number', 'first'),
+                        text=('text', 'sum'),
+                        jaccard_bigram=('2grams', 'first'),
+                        cosine_similarity=('cosine_similarity', 'max'),
+                        description_len=('description_len', 'median'),
+                        # agency=('agency', 'max'),
+                        url=('url', 'first')
+                        )
+                ).sort_values(by='cosine_similarity', ascending=False).head(3)
+        for index2, row2 in temp_df.iterrows():
+            # agency = row2['agency']
+            grant_title = row2['title']
+            url = row2['url']
+            text = row2['text']
+            cosine_similarity = row2['cosine_similarity']
+            new_row = {'company': company, 
+                       'project_title': title, 
+                       'summary': summary, 
+                    #    'agency': agency, 
+                       'grant_title': grant_title, 
+                       'text': text, 'url': url}
+            new_row_df = pd.DataFrame([new_row])
+            grant_recommendations = pd.concat([grant_recommendations, new_row_df], ignore_index=True)
+    st.session_state["grant_recommendations"] = True
 
 
-        st.write(temp_df[['title', 'topic_number', 'text', 'jaccard_bigram', 'jaccard_trigram', 'description_len', 'cosine_similarity', 'url']])
-        fig = px.scatter(temp_df, x='jaccard_bigram', y='cosine_similarity', trendline="ols", trendline_color_override='red', opacity=0.5)
-        st.plotly_chart(fig)
+if st.session_state["grant_recommendations"] == True:
+    st.divider()
+    st.subheader('Grant Recommendations')
+    st.write(grant_recommendations)
+
+
+    excel_file = convert_df_to_excel(grant_recommendations)
+    
+
+    if st.download_button("Press to Download", excel_file, "grant_recommendations.xlsx", "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", key='download-excel'):
+        st.session_state['download_excel'] = True
+    # csv = convert_df_to_csv(grant_recommendations)
+    # if st.download_button("Press to Download",csv,"grant_recommendations.csv","text/csv", key='download-csv'):
+    #     st.session_state['download_csv'] = True
+
+
+# if selected == 'Text Box':
+#     # Text Input
+#     query = st.text_area('Enter description:', max_chars=3000, key='query_text', height=300)
+
+#     col1, col2, col3, col4 = st.columns([3, 3, 5, 3])
+
+#     with col1:
+#         find_program = st.button('Find Programs')
+#     with col2:
+#         delete_abstract = st.button('Clear Keywords', on_click=clear_query)
+
+#     if find_program:
+#         st.session_state['text_box_button'] = True
+
+#     if st.session_state['text_box_button'] & len(query) > 0:
+#         # tokens and ngrams for abstract text
+#         query_embedding = get_embedding(query)
+#         tokens = tp.generate_tokens(text=query)
+#         unigrams = tp.generate_ngrams(tokens, 1)
+#         bigrams = tp.generate_ngrams(tokens, 2)
+#         trigrams = tp.generate_ngrams(tokens, 3)
+
+#         temp_df = (grants
+#                   .assign(
+#                       jaccard_unigram=grants['unigrams'].apply(lambda x: modified_jaccard(x, unigrams)),
+#                       jaccard_bigram=grants['bigrams'].apply(lambda x: modified_jaccard(x, bigrams)),
+#                       jaccard_trigram=grants['trigrams'].apply(lambda x: modified_jaccard(x, trigrams)),
+#                       cosine_similarity=grants['text_embedded'].apply(lambda y: np.dot(query_embedding, y)),
+#                       description_len=grants['text'].str.len()
+#                       )
+#                   .query('cosine_similarity >= 0.80 and jaccard_bigram != 0')
+#                   .groupby(['topic_number'], as_index=False)
+#                   .agg(title=('title', 'max'),
+#                        topic_number=('topic_number', 'max'),
+#                        text=('text', 'sum'),
+#                        jaccard_bigram=('jaccard_bigram', 'max'),
+#                        jaccard_trigram=('jaccard_trigram', 'max'),
+#                        cosine_similarity=('cosine_similarity', 'max'),
+#                        description_len=('description_len', 'median'),
+#                        url=('url', 'max')
+#                        )
+#                   ).sort_values(by='cosine_similarity', ascending=False)
+#         st.write(temp_df[['title', 'topic_number', 'text', 'jaccard_bigram', 'jaccard_trigram', 'description_len', 'cosine_similarity', 'url']])
+#         fig = px.scatter(temp_df, x='jaccard_bigram', y='cosine_similarity', trendline="ols", trendline_color_override='red', opacity=0.5)
+#         st.plotly_chart(fig)
 
 
 
